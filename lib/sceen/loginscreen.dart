326aacc8993b10ff35fdcf232bd7e4f4144f2a7b
kamruddeen21 <kamruddeenqureshi21@gmail.com>
2023-12-12 11:51:07 +0530

    logi button

--- lib/sceen/loginscreen.dart
+++ lib/sceen/loginscreen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:skill_project/sceen/homescreen.dart';
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+
+   LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Login Screen'),
+        title: const Text('Login Screen'),
       ),
     
       body: Padding(
@@ -16,8 +24,8 @@ class LoginScreen extends StatelessWidget {
         child: Column(
         
           children:  [
-             Center(),
-             Text('Welcome App',style: TextStyle(
+             const Center(),
+             const Text('Welcome App',style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
             ),),
@@ -31,46 +39,72 @@ class LoginScreen extends StatelessWidget {
   decoration: InputDecoration(
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
-      borderSide:  BorderSide(
+      borderSide:  const BorderSide(
         color: Colors.black,
       ),
     ),
-    prefixIcon:  Icon(Icons.email_outlined),
+    prefixIcon:  const Icon(Icons.email_outlined),
     hintText: 'Email',
   ),
 ),
 
-
-
-                SizedBox(height: 20,),
+                const SizedBox(height: 20,),
               TextFormField(
                 obscureText: true,
                 
                 decoration: InputDecoration(
                    border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide:  BorderSide(
+                    borderSide:  const BorderSide(
                       color: Colors.black,
                     )
                   ),
-                  prefixIcon: Icon(Icons.password_outlined),
+                  prefixIcon: const Icon(Icons.password_outlined),
                   hintText: 'Password',
                 ),
               ),
               const SizedBox(height: 20,),
-              ElevatedButton(
-   onPressed: () { 
-    Navigator.push(context, MaterialPageRoute(builder: (context){
-      return HomeScreen();
-    }));
-    },
-   child:const Text(
-    'Login',),
-    style:   TextButton.styleFrom(
-minimumSize: const Size(100, 50)
-    ),
+
+              InkWell(
+                onTap: () async{
+          setState((){
+            changeButton = true;
+          });
+          await Future.delayed(const Duration(seconds: 1));
+                  Navigator.push(context, MaterialPageRoute(builder: (context){
+    return const HomeScreen();
+   }));
+                },
+                child: AnimatedContainer(
+                  duration: const Duration(seconds: 1),
+                  height:50 ,width: changeButton? 50 :150,
+                 alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                     color: Colors.red,
+                    borderRadius: BorderRadius.circular(changeButton?50:10)
+                  ),
+                  
+                  child:changeButton?Icon(Icons.done,color: Colors.white,) : Text('Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ),
+              )
+   //           ElevatedButton(
+  // onPressed: () { 
+   // Navigator.push(context, MaterialPageRoute(builder: (context){
+     // return const HomeScreen();
+   // }));
+   // },
+    // child:const Text(
+    // 'Login',),
+   // style:   TextButton.styleFrom(
+// minimumSize: const Size(100, 50)
+  //  ),
    
-   ),
+//   ),
             
                 ],
               ),
@@ -83,4 +117,6 @@ minimumSize: const Size(100, 50)
 
     );
   }
+
 }
+
